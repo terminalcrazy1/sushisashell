d7cc17daf062b9fdad14944a78f2f84493529599
terminalcrazy1 <liam.jbm2020@gmail.com>
2024-10-05 09:30:18 -0400

    Blocked out functions with no code (Hopefully)

--- sush.c
+++ sush.c
@@ -2,12 +2,32 @@
 #include "config.h"
 #include <stdio.h>
 
+void sush_readin() {
+
+}
+
+void sush_splitin() {
+
+}
+
+void sush_executein() {
+
+}
+
+char* sushrc() {
+  /* Use sush_main to interpret commands contained within ./sushrc, */
+  /* akin to .bashrc or .zshrc */
+  return "Complete";
+}
+
 char* sush_main() {
   printf(PS1);
-  return "Success";
+  sush_readin();
+  sush_splitin();
+  return "Complete";
 }
 
 int main() {
+  sushrc();
   sush_main();
-  return 0;
 }
