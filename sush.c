7a886fa146ccc8df2f155c8d762f156d26c50712
Liam Morison <liam.jbm2020@gmail.com>
2024-11-10 12:00:58 -0500

    Basic logic for sush_main()

--- sush.c
+++ sush.c
@@ -1,1 +1,14 @@
-/* See LICENSE for license details */
+/* See LICENSE for license details
+ * This is sush. Sush is a simple shell written in C. The only goals of
+ * sush are to be minimally POSIX compliant and to
+ * follow the suckless philosophy.
+ */
+sush_main() {
+  if (onstart == 0) {
+    sush_setup()
+    onstart += 1
+  }
+    sush_readline()
+    sush_splitline()
+    sush_passline()
+}
