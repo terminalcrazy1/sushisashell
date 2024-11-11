3cc7641b897d693a06eabb572ff7d7d878c7c131
Liam Morison <liam.jbm2020@gmail.com>
2024-11-11 09:13:27 -0500

    Readline successful. Print to stdout. Tested to 102 c

--- sush.c
+++ sush.c
@@ -1,20 +1,25 @@
-/* See LICENSE for license details
+/* See LICENSE for license details. See LIMITATIONS for limitations.
  * This is sush. Sush is a simple shell written in C. The only goals of
  * sush are to be minimally POSIX compliant and to
  * follow the suckless philosophy.
  */
- #include <stdio.h>
-void sush_readline();
-char sush_setup();
-int onstart = 1;
-char *line;
+#include <stdio.h>
+#include <string.h>
+const int length_increment = 10;
+int length = 80;
+char line[80];
 void sush_readline() {
-  
+  while(1) {
+    fgets(line, length, stdin);
+    int position = strlen(line);
+    if (line[position] == '\n') {
+      line[position] = '\0';
+      return;
+    }
+  printf(line)
+  return;
+  }
 }
 int main() {
-  if (onstart == 1) {
-    sush_setup();
-    onstart = 0;
-  }
-    return 0;
+  sush_readline();
 }
