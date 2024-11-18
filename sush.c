86fc79bb7558abc04b121c4573b7221210578c98
terminalcrazy1 <liam.jbm2020@gmail.com>
2024-11-17 23:55:45 -0500

    Actually works now. Writes up to the tested 120 C from stdin to line[]

--- sush.c
+++ sush.c
@@ -1,20 +1,31 @@
-/* See LICENSE for license details
+/* See LICENSE for license details. See LIMITATIONS for limitations.
  * This is sush. Sush is a simple shell written in C. The only goals of
  * sush are to be minimally POSIX compliant and to
  * follow the suckless philosophy.
  */
- #include <stdio.h>
-void sush_readline();
-char sush_setup();
-int onstart = 1;
-char *line;
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <string.h>
+char line[80];
+int position = 0;
+int complete = 0;
 void sush_readline() {
-  
+  funlockfile(stdin);
+  while (complete == 0) {
+    char c = getchar_unlocked();
+    if (c == '\n') {
+      c = '\0';
+      line[position] = c;
+      printf(line, "\n");
+      printf("%lu", strlen(line));
+    } else {
+      line[position] = c;
+      position += 1;
+    }
+  }
+  flockfile(stdin);
 }
 int main() {
-  if (onstart == 1) {
-    sush_setup();
-    onstart = 0;
-  }
-    return 0;
+  setbuf(stdout, NULL);
+  sush_readline();
 }
