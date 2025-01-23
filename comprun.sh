cf78d728f69dc32a0c716aafd1ddea0e16911789
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-01-23 17:10:25 -0500

    Functioning parse, prints first word (command) to stdout

--- comprun.sh
+++ comprun.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 rm -rf a.out
-musl-gcc -pedantic-errors -std=c99 -Wall -Wextra -Werror -O3 sush.c
+gcc -pedantic-errors -std=c99 -Wall -Wextra -Werror -O3 sush.c
 ./a.out
--- sush.c
+++ sush.c
@@ -7,31 +7,44 @@
 #include <stdio.h>
 #include <string.h>
 char line[80];
+char parsed_line[80];
 int position = 0;
 int complete = 0;
 void sush_parseline() {
-  printf("Parse the variable 'line'")
-}
-void sush_readline() {
-  funlockfile(stdin);
-  while (complete == 0) {
-    char c = getchar_unlocked();
-    if (c == '\n') {
-      c = '\0';
-      line[position] = c;
-      printf(line, "\n");
-      printf("%lu", strlen(line));
-      complete = 1;
-    } else {
-      line[position] = c;
-      position += 1;
+	while (complete == 0) {
+		int c = line[position];
+		if (c == ' ') {
+			parsed_line[position] = '\0';
+			complete = 1;
+    } else if (c == '\n') {
+			parsed_line[position] = '\0';
+		} else {
+			parsed_line[position] = c;
+			position += 1;
     }
   }
-  flockfile(stdin);
-  sush_parseline();
+	printf(parsed_line);
+}
+void sush_readline() {
+	funlockfile(stdin);
+	while (complete == 0) {
+		char c = getchar_unlocked();
+		if (c == '\n') {
+			c = '\0';
+			line[position] = c;
+			complete = 1;
+    } else {
+			line[position] = c;
+			position += 1;
+    }
+  }
+	flockfile(stdin);
+	complete = 0;
+	position = 0;
+	sush_parseline();
 }
 int main() {
-  setbuf(stdout, NULL);
-  printf("> ");
-  sush_readline();
+	setbuf(stdout, NULL);
+	printf("> ");
+	sush_readline();
 }
